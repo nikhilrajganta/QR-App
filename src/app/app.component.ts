55b02f4214799f6da9c94db37f57e60d182decab
nikhilrajganta <nikhilraj.ganta@proclink.com>
2024-09-09 14:45:49 +0530

    modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { QrComponent } from './qr/qr.component';
-import { environment } from '../env';
 
 @Component({
   selector: 'app-root',
@@ -13,17 +12,6 @@ import { environment } from '../env';
 export class AppComponent {
   title = 'static';
 
-  ngOnInit() {
-    this.loadAdsenseScript();
-  }
-
-  loadAdsenseScript() {
-    const script = document.createElement('script');
-    script.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${environment.adsensePublisherId}`;
-    script.async = true;
-    script.crossOrigin = 'anonymous';
-    document.head.appendChild(script);
-  }
   opengithub() {
     window.open('https://github.com/nikhilrajganta/QR-App', '_blank');
   }
