de3042894d8fa43de2dade274a09ba798670f0b4
nikhilrajganta <nikhilraj.ganta@proclink.com>
2024-09-09 12:30:20 +0530

    changes for ads

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { QrComponent } from './qr/qr.component';
+import { environment } from '../env';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,15 @@ import { QrComponent } from './qr/qr.component';
 })
 export class AppComponent {
   title = 'static';
+  ngOnInit() {
+    this.loadAdsenseScript();
+  }
+
+  loadAdsenseScript() {
+    const script = document.createElement('script');
+    script.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${environment.adsensePublisherId}`;
+    script.async = true;
+    script.crossOrigin = 'anonymous';
+    document.head.appendChild(script);
+  }
 }
